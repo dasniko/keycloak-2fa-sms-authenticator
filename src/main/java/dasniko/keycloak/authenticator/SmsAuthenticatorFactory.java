dda8bc5a38b8d33b52a590fb23eb8deafffec613
Niko Köbler <niko@n-k.de>
2022-05-04 08:10:21 +0200

    move to KC18

--- src/main/java/dasniko/keycloak/authenticator/SmsAuthenticatorFactory.java
+++ src/main/java/dasniko/keycloak/authenticator/SmsAuthenticatorFactory.java
@@ -8,7 +8,6 @@ import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 import org.keycloak.provider.ProviderConfigProperty;
 
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -16,9 +15,11 @@ import java.util.List;
  */
 public class SmsAuthenticatorFactory implements AuthenticatorFactory {
 
+	public static final String PROVIDER_ID = "sms-authenticator";
+
 	@Override
 	public String getId() {
-		return "sms-authenticator";
+		return PROVIDER_ID;
 	}
 
 	@Override
@@ -48,16 +49,12 @@ public class SmsAuthenticatorFactory implements AuthenticatorFactory {
 
 	@Override
 	public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
-		return new AuthenticationExecutionModel.Requirement[] {
-			AuthenticationExecutionModel.Requirement.REQUIRED,
-			AuthenticationExecutionModel.Requirement.ALTERNATIVE,
-			AuthenticationExecutionModel.Requirement.DISABLED,
-		};
+		return REQUIREMENT_CHOICES;
 	}
 
 	@Override
 	public List<ProviderConfigProperty> getConfigProperties() {
-		return Arrays.asList(
+		return List.of(
 			new ProviderConfigProperty("length", "Code length", "The number of digits of the generated code.", ProviderConfigProperty.STRING_TYPE, 6),
 			new ProviderConfigProperty("ttl", "Time-to-live", "The time to live in seconds for the code to be valid.", ProviderConfigProperty.STRING_TYPE, "300"),
 			new ProviderConfigProperty("senderId", "SenderId", "The sender ID is displayed as the message sender on the receiving device.", ProviderConfigProperty.STRING_TYPE, "Keycloak"),
