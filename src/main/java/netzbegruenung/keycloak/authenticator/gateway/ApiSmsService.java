394a99db2f8c52f92eb81daaeaa90284881bc1b5
Sven Seeberg <mail@sven-seeberg.de>
2022-06-26 11:39:40 +0200

    SMS API: add country code & basic auth

--- src/main/java/netzbegruenung/keycloak/authenticator/gateway/ApiSmsService.java
+++ src/main/java/netzbegruenung/keycloak/authenticator/gateway/ApiSmsService.java
@@ -23,14 +23,15 @@ package netzbegruenung.keycloak.authenticator.gateway;
 
 import java.util.Map;
 import java.util.HashMap;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.net.URI;
 import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.Builder;
 import java.net.http.HttpResponse;
 import org.jboss.logging.Logger;
+import org.apache.commons.codec.binary.Base64;
 
 public class ApiSmsService implements SmsService{
 
@@ -41,6 +42,7 @@ public class ApiSmsService implements SmsService{
 	private final String apiuser;
 
 	private final String from;
+	private final String countrycode;
 
 	private final String apitokenattribute;
 	private final String messageattribute;
@@ -56,6 +58,7 @@ public class ApiSmsService implements SmsService{
 		apitoken = config.getOrDefault("apitoken", "");
 		apiuser = config.getOrDefault("apiuser", "");
 
+		countrycode = config.getOrDefault("countrycode", "");
 		from = config.get("senderId");
 
 		apitokenattribute = config.getOrDefault("apitokenattribute", "");
@@ -65,15 +68,32 @@ public class ApiSmsService implements SmsService{
 	}
 
 	public void send(String phoneNumber, String message) {
-		LOG.info(String.format("Sending SMS Code to %s", phoneNumber));
+		phoneNumber = clean_phone_number(phoneNumber, countrycode);
+		Builder request_builder;
 		if (urlencode) {
-			send_urlencoded(phoneNumber, message);
+			request_builder = urlencoded_request(phoneNumber, message);
 		} else {
-			send_json(phoneNumber, message);
+			request_builder = json_request(phoneNumber, message);
+		}
+
+		HttpRequest request;
+		if (apiuser != "") {
+			request = request_builder.setHeader("Authorization", get_auth_header(apiuser, apitoken)).build();
+		} else {
+			request = request_builder.build();
+		}
+
+		var client = HttpClient.newHttpClient();
+		try {
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			LOG.warn(String.format("Sent SMS to %s; API response: %s", phoneNumber, response.toString()));
+		} catch (Exception e){
+			LOG.warn(String.format("Failed to send message to %s with request %s", apiurl, request.toString()));
+			e.printStackTrace();
 		}
 	}
 
-	public void send_json(String phoneNumber, String message) {
+	public Builder json_request(String phoneNumber, String message) {
 		String sendJson = "{"
 			.concat(apitokenattribute != "" ? String.format("\"%s\":\"%s\",", apitokenattribute, apitoken): "")
 			.concat(String.format("\"%s\":\"%s\",", messageattribute, message))
@@ -81,27 +101,13 @@ public class ApiSmsService implements SmsService{
 			.concat(String.format("\"%s\":\"%s\"", senderattribute, from))
 			.concat("}");
 
-		var request = HttpRequest.newBuilder()
+		 return HttpRequest.newBuilder()
 			.uri(URI.create(apiurl))
 			.header("Content-Type", "application/json")
-			.POST(HttpRequest.BodyPublishers.ofString(sendJson))
-			.build();
-
-        var client = HttpClient.newHttpClient();
-
-        HttpResponse<String> response;
-		try {
-			response = client.send(request, HttpResponse.BodyHandlers.ofString());
-		} catch (IOException e) {
-			LOG.warn(String.format("Failed to send message to %s with body %s", apiurl, sendJson));
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			LOG.warn(String.format("Failed to send message to %s with body %s", apiurl, sendJson));
-			e.printStackTrace();
-		}
+			.POST(HttpRequest.BodyPublishers.ofString(sendJson));
 	}
 
-	public void send_urlencoded(String phoneNumber, String message) {
+	public Builder urlencoded_request(String phoneNumber, String message) {
 		Map<String, String> formData = new HashMap<>();
 		if (apitokenattribute != "") {
 			formData.put(apitokenattribute, apitoken);
@@ -109,21 +115,11 @@ public class ApiSmsService implements SmsService{
 		formData.put(messageattribute, message);
 		formData.put(receiverattribute, phoneNumber);
 		formData.put(senderattribute, from);
+		String form_data = getFormDataAsString(formData);
 
-		var client = HttpClient.newHttpClient();
-		var form_data = getFormDataAsString(formData);
-		var request = HttpRequest.newBuilder(URI.create(apiurl))
-			.POST(HttpRequest.BodyPublishers.ofString(form_data))
-			.build();
-		try {
-			client.send(request, HttpResponse.BodyHandlers.ofString());
-		} catch (IOException e) {
-			LOG.warn(String.format("Failed to send message to %s with params %s", apiurl, form_data));
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			LOG.warn(String.format("Failed to send message to %s with params %s", apiurl, form_data));
-			e.printStackTrace();
-		}
+		return HttpRequest.newBuilder(URI.create(apiurl))
+				.POST(HttpRequest.BodyPublishers.ofString(form_data))
+				.setHeader("Authorization", get_auth_header(apiuser, apitoken));
 	}
 
 	private static String getFormDataAsString(Map<String, String> formData) {
@@ -137,5 +133,20 @@ public class ApiSmsService implements SmsService{
 			formBodyBuilder.append(URLEncoder.encode(singleEntry.getValue(), StandardCharsets.UTF_8));
 		}
 		return formBodyBuilder.toString();
+	}
+
+	private static String get_auth_header(String apiuser, String apitoken) {
+		String authString = apiuser + ":" + apitoken;
+		return String.format("Basic %s" + new String(Base64.encodeBase64(authString.getBytes())));
+	}
+
+	private static String clean_phone_number(String phone_number, String countrycode) {
+		if (phone_number.startsWith("00")) {
+			return phone_number.replaceFirst("00", countrycode);
+		}
+		if (phone_number.startsWith("0")) {
+			return phone_number.replaceFirst("0", countrycode);
+		}
+		return phone_number;
 	}
 }
